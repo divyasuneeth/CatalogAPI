6e9141e8f2752b8086c51383920548d8bc38fa31
Divya Suneeth <divyaprabhakar08@gmail.com>
2019-07-11 14:20:57 +0530

    adding code to edit item

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, ListItems
+from database_setup import Base, Category, ListItems,User
 
 app= Flask(__name__)
 
@@ -17,10 +17,10 @@ categories = session.query(Category).order_by(asc(Category.name))
 @app.route('/')
 def showlistItems():
     items = session.query(ListItems).order_by(asc(ListItems.name))
-    return render_template('lists.html', categories = categories,items=items)
+    return render_template('list_private.html', categories = categories,items=items)
 
 
-@app.route('/catalog/<string:catalogItem>/items')
+@app.route('/catalog/<string:catalogItem>')
 def showCatalogItems(catalogItem):
     try:
         c_item= session.query(Category).filter_by(name=catalogItem).one()
@@ -34,6 +34,15 @@ def showCatalogItems(catalogItem):
 def showItemDescription(catalogItem,item):
     item=session.query(ListItems).filter_by(name=item).first()
     return  render_template('description.html',categories=categories,item=item)  #"this page will "+item+" description in the catalog "+ catalogItem
+
+@app.route('/catalog/new')
+def addNewItem():
+    return render_template('additem.html',categories=categories)
+
+@app.route('/catalog/<int:itemid>/edit')
+def editItem(itemid):
+    item=session.query(ListItems).filter_by(id=itemid).first()
+    return render_template('editItem.html',categories=categories,item=item)
 
 if __name__ =='__main__':
     app.debug=True
--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,18 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(100), nullable=False)
+    picture = Column(String(250))
+
+
+
+
+
 class Category(Base):
     __tablename__='category'
 
